9cef4b6a9133c4674bfe19df5c7a617790658197
Leonardo Feitosa <leonardofeitosa3010@gmail.com>
2020-05-11 09:20:19 -0300

    Implementando a estrutura basica

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Frases do Dia"),
+        backgroundColor: Colors.green,
+      ),
+    );
+  }
+}
