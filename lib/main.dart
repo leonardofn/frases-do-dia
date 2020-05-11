952307e5230a8f245e42a0253e4a7f442255e74a
Leonardo Feitosa <leonardofeitosa3010@gmail.com>
2020-05-11 10:13:08 -0300

    Adicionando a logica para gerar uma frase aleatoria

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MaterialApp(
@@ -13,6 +14,22 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  var _frases = [
+    "Sou apenas um pequeno planeta que se perde diariamente em todo o seu universo.",
+    "Novas amizades serão sempre bem-vindas para darem cor e alegria ao meu dia a dia.",
+    "Gratidão não é pagamento, mas um reconhecimento que se demonstra no dia a dia.",
+    "Nem toda mudança importante acontece de repente e faz barulho, algumas são silenciosas e vão se fazendo no dia a dia."
+  ];
+
+  var _fraseGerada = "Clique abaixo para gerar uma frase!";
+
+  void gerarFrase() {
+    var numeroSorteado = Random().nextInt(_frases.length);
+    setState(() {
+      _fraseGerada = _frases[numeroSorteado];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,7 +63,7 @@ class _HomeState extends State<Home> {
                     ),
                   ),
                   color: Colors.green,
-                  onPressed: () {}),
+                  onPressed: gerarFrase),
             ],
           ),
         ),
